0db315872410c3d015ba5b2365bd215ee3148425
Lea Harvin <lnharvin@gmail.com>
2022-04-03 13:55:17 -0400

    adding copied files to Mapping Single Points branch

---
+++ Mapping_Single_Points/static/js/logic.js
@@ -0,0 +1,39 @@
+// Add console.log to check to see if our code is working.
+console.log("working");
+
+// Add a Map Object
+// Create the map object with a center and zoom level.
+let map = L.map('mapid').setView([40.7, -94.5], 4);
+
+// // An alternative to using the setView() method is to modify each attribute in the map object using the curly braces notation as follows: 
+// // Create the map object with a center and zoom level.
+// let map = L.map("mapid", {
+//     center: [
+//       40.7, -94.5
+//     ],
+//     zoom: 4
+//   });
+//   // This method is useful when we need to add multiple tile layers, or a background image of our map(s)
+
+// Add a Tile Layer for Our Map
+
+
+// We create the tile layer that will be the background of our map.
+// // started with ...
+// let streets = L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+//     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+//     maxZoom: 18,
+//     id: 'mapbox/streets-v11',
+//     tileSize: 512,
+//     zoomOffset: -1,
+//     accessToken: API_KEY
+// });
+
+// modified to Use the Mapbox Styles API
+let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+    maxZoom: 18,
+    accessToken: API_KEY
+});
+// Then we add our 'graymap' tile layer to the map.
+streets.addTo(map);
